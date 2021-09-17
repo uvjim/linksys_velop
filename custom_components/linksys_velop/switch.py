90a07c53dc691fcb17d108f1d1838fde9ca19b45
uvjim <uvjim@users.noreply.github.com>
2021-09-17 13:42:21 +0100

    Add support for changing guest Wi-Fi state
    Minimum version of pyvelop has been bumped to 2021.9.4

--- custom_components/linksys_velop/switch.py
+++ custom_components/linksys_velop/switch.py
@@ -25,6 +25,7 @@ async def async_setup_entry(hass: HomeAssistant, config: ConfigEntry, async_add_
     """Setup the switches from a config entry"""
 
     switch_classes = [
+        LinksysVelopMeshGuestWiFiSwitch,
         LinksysVelopMeshParentalControlSwitch,
     ]
 
@@ -84,6 +85,62 @@ class LinksysVelopMeshParentalControlSwitch(LinksysVelopSwitch):
         """Returns the icon for the switch"""
 
         return "hass:account" if self.is_on else "hass:account-off"
+
+    @property
+    def is_on(self) -> bool:
+        """Returns True if the switch is on, False otherwise"""
+
+        return self._state_value
+
+
+# noinspection PyAbstractClass
+class LinksysVelopMeshGuestWiFiSwitch(LinksysVelopSwitch):
+    """Representation of the switch entity for the guest Wi-Fi state"""
+
+    _attribute = "Guest Wi-Fi"
+    _attr_device_class = DEVICE_CLASS_SWITCH
+    _state_value: bool = False
+
+    @callback
+    async def async_update_callback(self, _: Union[datetime, None] = None):
+        """Update the state of the switch"""
+
+        self._state_value = self._mesh.guest_wifi_enabled
+        self.async_schedule_update_ha_state()
+
+    async def async_added_to_hass(self) -> None:
+        """Register callbacks and set initial status"""
+
+        self._state_value = self._mesh.guest_wifi_enabled
+        self.async_on_remove(
+            async_dispatcher_connect(
+                hass=self.hass,
+                signal=SIGNAL_UPDATE_PARENTAL_CONTROL_STATUS,
+                target=self.async_update_callback,
+            )
+        )
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Turn the switch off"""
+
+        to_state: bool = False
+        await self._mesh.async_set_guest_wifi_state(state=to_state)
+        self._state_value = to_state
+        self.async_schedule_update_ha_state()
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Turn the switch on"""
+
+        to_state: bool = True
+        await self._mesh.async_set_guest_wifi_state(state=to_state)
+        self._state_value = to_state
+        self.async_schedule_update_ha_state()
+
+    @property
+    def icon(self) -> str:
+        """Returns the icon for the switch"""
+
+        return "hass:wifi" if self.is_on else "hass:wifi-off"
 
     @property
     def is_on(self) -> bool:
