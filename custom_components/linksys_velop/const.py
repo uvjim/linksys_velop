b7a507d2d783fcdbb48ca162b2913487d1c6c0a9
uvjim <uvjim@users.noreply.github.com>
2022-04-12 17:27:22 +0100

    use typing in constants

--- custom_components/linksys_velop/const.py
+++ custom_components/linksys_velop/const.py
@@ -1,39 +1,45 @@
 """Constants for Linksys Velop"""
 
+from typing import (
+    List,
+    Optional,
+)
+
 from homeassistant.components.binary_sensor import DOMAIN as BINARY_SENSOR_DOMAIN
 from homeassistant.components.button import DOMAIN as BUTTON_DOMAIN
 from homeassistant.components.device_tracker import DOMAIN as DEVICE_TRACKER_DOMAIN
 from homeassistant.components.select import DOMAIN as SELECT_DOMAIN
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
+
 try:
     from homeassistant.components.update import DOMAIN as UPDATE_DOMAIN
 except ImportError:
     UPDATE_DOMAIN = None
 
-DOMAIN = "linksys_velop"
-ENTITY_SLUG = "Velop"
+DOMAIN: str = "linksys_velop"
+ENTITY_SLUG: str = "Velop"
 
-CONF_API_REQUEST_TIMEOUT = "api_request_timeout"
-CONF_COORDINATOR = "coordinator"
-CONF_COORDINATOR_MESH = "mesh"
-CONF_DEVICE_TRACKERS = "tracked"
-CONF_ENTRY_RELOAD = "reloading"
-CONF_FLOW_NAME = "name"
-CONF_NODE = "node"
-CONF_NODE_IMAGES = "node_images"
-CONF_SCAN_INTERVAL_DEVICE_TRACKER = "scan_interval_device_tracker"
-CONF_SERVICES_HANDLER = "services_handler"
-CONF_TITLE_PLACEHOLDERS = "title_placeholders"
-CONF_UNSUB_UPDATE_LISTENER = "unsub_update_listener"
+CONF_API_REQUEST_TIMEOUT: str = "api_request_timeout"
+CONF_COORDINATOR: str = "coordinator"
+CONF_COORDINATOR_MESH: str = "mesh"
+CONF_DEVICE_TRACKERS: str = "tracked"
+CONF_ENTRY_RELOAD: str = "reloading"
+CONF_FLOW_NAME: str = "name"
+CONF_NODE: str = "node"
+CONF_NODE_IMAGES: str = "node_images"
+CONF_SCAN_INTERVAL_DEVICE_TRACKER: str = "scan_interval_device_tracker"
+CONF_SERVICES_HANDLER: str = "services_handler"
+CONF_TITLE_PLACEHOLDERS: str = "title_placeholders"
+CONF_UNSUB_UPDATE_LISTENER: str = "unsub_update_listener"
 
-DEF_API_REQUEST_TIMEOUT = 10
-DEF_CONSIDER_HOME = 180
-DEF_FLOW_NAME = "Linksys Velop Mesh"
-DEF_SCAN_INTERVAL = 30
-DEF_SCAN_INTERVAL_DEVICE_TRACKER = 10
+DEF_API_REQUEST_TIMEOUT: int = 10
+DEF_CONSIDER_HOME: int = 180
+DEF_FLOW_NAME: str = "Linksys Velop Mesh"
+DEF_SCAN_INTERVAL: int = 30
+DEF_SCAN_INTERVAL_DEVICE_TRACKER: int = 10
 
-PLATFORMS = [
+PLATFORMS: List[Optional[str]] = [
     BINARY_SENSOR_DOMAIN,
     BUTTON_DOMAIN,
     DEVICE_TRACKER_DOMAIN,
@@ -43,12 +49,12 @@ PLATFORMS = [
     UPDATE_DOMAIN,
 ]
 
-SIGNAL_UPDATE_DEVICE_TRACKER = "update_device_tracker"
-SIGNAL_UPDATE_SPEEDTEST_RESULTS = "update_speedtest_results"
-SIGNAL_UPDATE_SPEEDTEST_STATUS = "update_speedtest_status"
+SIGNAL_UPDATE_DEVICE_TRACKER: str = "update_device_tracker"
+SIGNAL_UPDATE_SPEEDTEST_RESULTS: str = "update_speedtest_results"
+SIGNAL_UPDATE_SPEEDTEST_STATUS: str = "update_speedtest_status"
 
-ST_IGD = "urn:schemas-upnp-org:device:InternetGatewayDevice:2"
+ST_IGD: str = "urn:schemas-upnp-org:device:InternetGatewayDevice:2"
 
-STEP_USER = "user"
-STEP_TIMERS = "timers"
-STEP_DEVICE_TRACKERS = "device_trackers"
+STEP_USER: str = "user"
+STEP_TIMERS: str = "timers"
+STEP_DEVICE_TRACKERS: str = "device_trackers"
