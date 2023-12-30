c28d3a8cebb4aea972a7f17bdd94b7ad64b4128b
uvjim <git@uvjim.co.uk>
2023-12-30 20:17:46 +0000

    switch entity

--- custom_components/linksys_velop/switch.py
+++ custom_components/linksys_velop/switch.py
@@ -37,33 +37,18 @@ _LOGGER = logging.getLogger(__name__)
 
 
 # region #-- switch entity descriptions --#
-@dataclasses.dataclass
-class OptionalLinksysVelopDescription:
-    """Represent the optional attributes of the switch description."""
+@dataclasses.dataclass(frozen=True)
+class AdditionalSwitchDescription:
+    """Represent the additional attributes of the switch description."""
 
+    turn_off: str
+    turn_on: str
     extra_attributes: Callable | None = None
     icon_off: str | None = None
     icon_on: str | None = None
     state_value: Callable | None = None
     turn_off_args: dict | None = dict
     turn_on_args: dict | None = dict
-
-
-@dataclasses.dataclass
-class RequiredLinksysVelopDescription:
-    """Represent the required attributes of the switch description."""
-
-    turn_off: str
-    turn_on: str
-
-
-@dataclasses.dataclass
-class LinksysVelopSwitchDescription(
-    OptionalLinksysVelopDescription,
-    SwitchEntityDescription,
-    RequiredLinksysVelopDescription,
-):
-    """Describes switch entity."""
 
 
 # endregion
@@ -80,8 +65,10 @@ def _device_internet_access_state(device_details: Device) -> bool | None:
 
     if len(blocked_internet_access.values()) == 0:
         return True
-    
-    return not all(",".join(hrs) == "00:00-00:00" for hrs in blocked_internet_access.values())
+
+    return not all(
+        ",".join(hrs) == "00:00-00:00" for hrs in blocked_internet_access.values()
+    )
 
 
 async def async_setup_entry(
@@ -95,127 +82,142 @@ async def async_setup_entry(
     switches_to_remove: List[LinksysVelopMeshSwitch] = []
 
     # region #-- Device switches --#
-    device_switch_descriptions: tuple[LinksysVelopSwitchDescription, ...]
     for device_id in config_entry.options.get(CONF_DEVICE_UI, []):
-        device_switch_descriptions = (
-            LinksysVelopSwitchDescription(
-                icon_off="mdi:web-off",
-                icon_on="mdi:web",
-                key="",
-                name="Internet Access",
-                state_value=_device_internet_access_state,
-                translation_key="internet_access",
-                turn_off="async_set_parental_control_rules",
-                turn_off_args=lambda d: {
-                    "device_id": d.unique_id,
-                    "rules": dict(
-                        map(
-                            lambda weekday, readable_schedule: (
-                                weekday.name,
-                                readable_schedule,
-                            ),
-                            ParentalControl.WEEKDAYS,
-                            ("00:00-00:00",) * len(ParentalControl.WEEKDAYS),
-                        )
-                    ),
-                },
-                turn_on="async_set_parental_control_rules",
-                turn_on_args=lambda d: {"device_id": d.unique_id, "rules": {}},
-            ),
-        )
-
-        for switch_description in device_switch_descriptions:
-            switches.append(
+        switches.extend(
+            [
                 LinksysVelopDeviceSwitch(
+                    additional_description=AdditionalSwitchDescription(
+                        icon_off="mdi:web-off",
+                        icon_on="mdi:web",
+                        state_value=_device_internet_access_state,
+                        turn_off="async_set_parental_control_rules",
+                        turn_off_args=lambda d: {
+                            "device_id": d.unique_id,
+                            "rules": dict(
+                                map(
+                                    lambda weekday, readable_schedule: (
+                                        weekday.name,
+                                        readable_schedule,
+                                    ),
+                                    ParentalControl.WEEKDAYS,
+                                    ("00:00-00:00",) * len(ParentalControl.WEEKDAYS),
+                                )
+                            ),
+                        },
+                        turn_on="async_set_parental_control_rules",
+                        turn_on_args=lambda d: {"device_id": d.unique_id, "rules": {}},
+                    ),
                     config_entry=config_entry,
                     coordinator=coordinator,
-                    description=switch_description,
+                    description=SwitchEntityDescription(
+                        key="",
+                        name="Internet Access",
+                        translation_key="internet_access",
+                    ),
                     device_id=device_id,
                 )
-            )
+            ]
+        )
     # endregion
 
     # region #-- Mesh switches --#
-    mesh_switch_descriptions: tuple[LinksysVelopSwitchDescription, ...] = (
-        LinksysVelopSwitchDescription(
-            extra_attributes=lambda m: {
-                f"network {idx}": network
-                for idx, network in enumerate(m.guest_wifi_details)
-            },
-            icon_off="hass:wifi-off",
-            icon_on="hass:wifi",
-            key="guest_wifi_enabled",
-            name="Guest Wi-Fi",
-            translation_key="guest_wifi",
-            turn_off="async_set_guest_wifi_state",
-            turn_off_args={
-                "state": False,
-            },
-            turn_on="async_set_guest_wifi_state",
-            turn_on_args={
-                "state": True,
-            },
-        ),
-        LinksysVelopSwitchDescription(
-            key="homekit_enabled",
-            name="HomeKit Integration",
-            translation_key="homekit",
-            turn_off="async_set_homekit_state",
-            turn_off_args={
-                "state": False,
-            },
-            turn_on="async_set_homekit_state",
-            turn_on_args={
-                "state": True,
-            },
-        ),
-        LinksysVelopSwitchDescription(
-            extra_attributes=lambda m: (
-                {
-                    "rules": {
-                        device.name: device.parental_control_schedule
-                        for device in m.devices
-                        if device.parental_control_schedule
-                    }
-                }
-            ),
-            icon_off="hass:account-off",
-            icon_on="hass:account",
-            key="parental_control_enabled",
-            name="Parental Control",
-            translation_key="parental_control",
-            turn_off="async_set_parental_control_state",
-            turn_off_args={
-                "state": False,
-            },
-            turn_on="async_set_parental_control_state",
-            turn_on_args={
-                "state": True,
-            },
-        ),
-        LinksysVelopSwitchDescription(
-            key="wps_state",
-            name="WPS",
-            translation_key="wps",
-            turn_off="async_set_wps_state",
-            turn_off_args={
-                "state": False,
-            },
-            turn_on="async_set_wps_state",
-            turn_on_args={
-                "state": True,
-            },
-        ),
-    )
-
-    for switch_description in mesh_switch_descriptions:
-        switches.append(
+    switches.extend(
+        [
             LinksysVelopMeshSwitch(
+                additional_description=AdditionalSwitchDescription(
+                    extra_attributes=lambda m: {
+                        f"network {idx}": network
+                        for idx, network in enumerate(m.guest_wifi_details)
+                    },
+                    icon_off="hass:wifi-off",
+                    icon_on="hass:wifi",
+                    turn_off="async_set_guest_wifi_state",
+                    turn_off_args={
+                        "state": False,
+                    },
+                    turn_on="async_set_guest_wifi_state",
+                    turn_on_args={
+                        "state": True,
+                    },
+                ),
                 config_entry=config_entry,
                 coordinator=coordinator,
-                description=switch_description,
-            )
-        )
+                description=SwitchEntityDescription(
+                    key="guest_wifi_enabled",
+                    name="Guest Wi-Fi",
+                    translation_key="guest_wifi",
+                ),
+            ),
+            LinksysVelopMeshSwitch(
+                additional_description=AdditionalSwitchDescription(
+                    turn_off="async_set_homekit_state",
+                    turn_off_args={
+                        "state": False,
+                    },
+                    turn_on="async_set_homekit_state",
+                    turn_on_args={
+                        "state": True,
+                    },
+                ),
+                config_entry=config_entry,
+                coordinator=coordinator,
+                description=SwitchEntityDescription(
+                    key="homekit_enabled",
+                    name="HomeKit Integration",
+                    translation_key="homekit",
+                ),
+            ),
+            LinksysVelopMeshSwitch(
+                additional_description=AdditionalSwitchDescription(
+                    extra_attributes=lambda m: (
+                        {
+                            "rules": {
+                                device.name: device.parental_control_schedule
+                                for device in m.devices
+                                if device.parental_control_schedule
+                            }
+                        }
+                    ),
+                    icon_off="hass:account-off",
+                    icon_on="hass:account",
+                    turn_off="async_set_parental_control_state",
+                    turn_off_args={
+                        "state": False,
+                    },
+                    turn_on="async_set_parental_control_state",
+                    turn_on_args={
+                        "state": True,
+                    },
+                ),
+                config_entry=config_entry,
+                coordinator=coordinator,
+                description=SwitchEntityDescription(
+                    key="parental_control_enabled",
+                    name="Parental Control",
+                    translation_key="parental_control",
+                ),
+            ),
+            LinksysVelopMeshSwitch(
+                additional_description=AdditionalSwitchDescription(
+                    turn_off="async_set_wps_state",
+                    turn_off_args={
+                        "state": False,
+                    },
+                    turn_on="async_set_wps_state",
+                    turn_on_args={
+                        "state": True,
+                    },
+                ),
+                config_entry=config_entry,
+                coordinator=coordinator,
+                description=SwitchEntityDescription(
+                    key="wps_state",
+                    name="WPS",
+                    translation_key="wps",
+                ),
+            ),
+        ]
+    )
     # endregion
 
     async_add_entities(switches)
@@ -229,16 +231,18 @@ async def async_setup_entry(
 class LinksysVelopDeviceSwitch(LinksysVelopDeviceEntity, SwitchEntity, ABC):
     """Representation of a device switch."""
 
-    entity_description: LinksysVelopSwitchDescription
-
     def __init__(
         self,
         coordinator: DataUpdateCoordinator,
         config_entry: ConfigEntry,
-        description: LinksysVelopSwitchDescription,
+        description: SwitchEntityDescription,
         device_id: str,
+        additional_description: AdditionalSwitchDescription | None = None,
     ) -> None:
         """Initialise."""
+        self._additional_description: AdditionalSwitchDescription | None = (
+            additional_description
+        )
         self._attr_device_class = SwitchDeviceClass.SWITCH
         self._attr_entity_category = EntityCategory.CONFIG
         self._value: bool | None = None
@@ -256,8 +260,8 @@ class LinksysVelopDeviceSwitch(LinksysVelopDeviceEntity, SwitchEntity, ABC):
         if device_id is not None:
             self._update_device_id(device_id=device_id)
 
-        if isinstance(self.entity_description.state_value, Callable):
-            self._value = self.entity_description.state_value(self._device)
+        if isinstance(self._additional_description.state_value, Callable):
+            self._value = self._additional_description.state_value(self._device)
         else:
             self._value = getattr(self._device, self.entity_description.key, None)
 
@@ -282,11 +286,11 @@ class LinksysVelopDeviceSwitch(LinksysVelopDeviceEntity, SwitchEntity, ABC):
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn the switch off."""
-        action = getattr(self._mesh, self.entity_description.turn_off)
-        if isinstance(self.entity_description.turn_off_args, Callable):
-            action_args = self.entity_description.turn_off_args(self._device)
+        action = getattr(self._mesh, self._additional_description.turn_off)
+        if isinstance(self._additional_description.turn_off_args, Callable):
+            action_args = self._additional_description.turn_off_args(self._device)
         else:
-            action_args = self.entity_description.turn_off_args
+            action_args = self._additional_description.turn_off_args
         if isinstance(action, Callable):
             await action(**action_args)
             self._value = False
@@ -295,11 +299,11 @@ class LinksysVelopDeviceSwitch(LinksysVelopDeviceEntity, SwitchEntity, ABC):
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn the switch on."""
-        action = getattr(self._mesh, self.entity_description.turn_on)
-        if isinstance(self.entity_description.turn_on_args, Callable):
-            action_args = self.entity_description.turn_on_args(self._device)
+        action = getattr(self._mesh, self._additional_description.turn_on)
+        if isinstance(self._additional_description.turn_on_args, Callable):
+            action_args = self._additional_description.turn_on_args(self._device)
         else:
-            action_args = self.entity_description.turn_on_args
+            action_args = self._additional_description.turn_on_args
         if isinstance(action, Callable):
             await action(**action_args)
             self._value = True
@@ -309,10 +313,10 @@ class LinksysVelopDeviceSwitch(LinksysVelopDeviceEntity, SwitchEntity, ABC):
     @property
     def icon(self) -> str | None:
         """Get the icon."""
-        if self.entity_description.icon_on and self.is_on:
-            return self.entity_description.icon_on
+        if self._additional_description.icon_on and self.is_on:
+            return self._additional_description.icon_on
 
-        return self.entity_description.icon_off
+        return self._additional_description.icon_off
 
     @property
     def is_on(self) -> bool | None:
@@ -323,15 +327,17 @@ class LinksysVelopDeviceSwitch(LinksysVelopDeviceEntity, SwitchEntity, ABC):
 class LinksysVelopMeshSwitch(LinksysVelopMeshEntity, SwitchEntity, ABC):
     """Representation of Mesh switch."""
 
-    entity_description: LinksysVelopSwitchDescription
-
     def __init__(
         self,
         coordinator: DataUpdateCoordinator,
         config_entry: ConfigEntry,
-        description: LinksysVelopSwitchDescription,
+        description: SwitchEntityDescription,
+        additional_description: AdditionalSwitchDescription | None = None,
     ) -> None:
         """Initialise."""
+        self._additional_description: AdditionalSwitchDescription | None = (
+            additional_description
+        )
         self._attr_device_class = SwitchDeviceClass.SWITCH
         self._attr_entity_category = EntityCategory.CONFIG
         self.entity_domain = ENTITY_DOMAIN
@@ -353,27 +359,27 @@ class LinksysVelopMeshSwitch(LinksysVelopMeshEntity, SwitchEntity, ABC):
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn the switch off."""
-        action = getattr(self._mesh, self.entity_description.turn_off)
+        action = getattr(self._mesh, self._additional_description.turn_off)
         if isinstance(action, Callable):
-            await action(**self.entity_description.turn_off_args)
+            await action(**self._additional_description.turn_off_args)
             self._value = False
             self.async_schedule_update_ha_state()
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn the switch on."""
-        action = getattr(self._mesh, self.entity_description.turn_on)
+        action = getattr(self._mesh, self._additional_description.turn_on)
         if isinstance(action, Callable):
-            await action(**self.entity_description.turn_on_args)
+            await action(**self._additional_description.turn_on_args)
             self._value = True
             self.async_schedule_update_ha_state()
 
     @property
     def icon(self) -> str | None:
         """Get the icon."""
-        if self.entity_description.icon_on and self.is_on:
-            return self.entity_description.icon_on
+        if self._additional_description.icon_on and self.is_on:
+            return self._additional_description.icon_on
 
-        return self.entity_description.icon_off
+        return self._additional_description.icon_off
 
     @property
     def is_on(self) -> bool | None:
